ed4e9966bdd86ed45fe31d23a3989f292f3ae42f
Promiseejikeme <103903590+Promiseejikeme@users.noreply.github.com>
2022-04-18 11:39:40 +0100

    Add files via upload

---
+++ INDEX.js
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width">
+  <title>replit</title>
+  <link href="style.css" rel="stylesheet" type="text/css" />
+</head>
+
+<body>
+  <h1>Hello World</h1>
+  <h2>BetterFarmingApp</h2>
+  <h3>Welcome to the BetterFarmingApp</h3>
+  <main>
+    <img src="https://img-global.cpcdn.com/recipes/a7810d6a3e9ab699/1200x630cq70/photo.jpg" width = "500" alt = "Image not currently available">
+    <link href = "style.css" rel = "stylesheet" type = "text/css" />
+  <p> As an Igbo man fronm the south eastern part of Nigeria I am so much in love with the roasted yam, it is my best dish with any sauce or just served hot</p>
+  <p> I was born and raised in Lafia, Nasarawa State in Nigeria, though I am originally from the eastern part of the country, I had my primary school studies at the Presbytarian Nursery and Primary school and completed it at Methodist Nursery and primary school and then went on to have my secondary scchool education at the kings and queens comprehensive college, Lafia, in 2016, I gained admission into the University of Nigeria, Nsukka to study Agricultural and Bioresources enginering in the faculty of Engineering.
+  before my admission, I served as a primary school teacher for two years before leaving to further my studies at the University</p>
+  </main>
+ 
+  <!-- I am usually scared of crowds, I usually shake when adressing a large crowd-->
+  <script src="script.js"></script>
+</body>
+
+</html>
